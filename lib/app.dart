9a1510d4b1deed8bfb27803f3d6486c50d8aaf80
mahin <mdmahinurrahmannifad@gmail.com>
2024-12-26 00:19:15 +0600

    Sign in, Signup and password recovery screens are added.

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_flutter/ui/screens/forgot_password_verify_email_screen.dart';
+import 'package:task_manager_flutter/ui/screens/forgot_password_verify_otp_screen.dart';
+import 'package:task_manager_flutter/ui/screens/main_bottom_nav_screen.dart';
+import 'package:task_manager_flutter/ui/screens/reset_password_screen.dart';
 import 'package:task_manager_flutter/ui/screens/sign_in_screen.dart';
+import 'package:task_manager_flutter/ui/screens/sign_up_screen.dart';
 import 'package:task_manager_flutter/ui/screens/splash_screen.dart';
+import 'package:task_manager_flutter/ui/utils/app_color.dart';
 
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
@@ -8,15 +14,68 @@ class TaskManagerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        initialRoute: '/',
-        onGenerateRoute: (RouteSettings settings) {
-          late Widget widget;
-          if (settings.name == SplashScreen.name) {
-            widget = const SplashScreen();
-          } else if (settings.name == SignInScreen.name) {
-            widget = const SignInScreen();
-          }
-          return MaterialPageRoute(builder: (_) => widget);
-        });
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.themeColor,
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+          titleSmall: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+          fillColor: Colors.white,
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.w400,
+            color: Colors.grey.shade800,
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.themeColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              fixedSize: const Size.fromWidth(double.maxFinite),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 16,
+              ),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 16)),
+        ),
+      ),
+      onGenerateRoute: (RouteSettings settings) {
+        late Widget widget;
+        if (settings.name == SplashScreen.name) {
+          widget = const SplashScreen();
+        } else if (settings.name == SignInScreen.name) {
+          widget = const SignInScreen();
+        } else if (settings.name == SignUpScreen.name) {
+          widget = const SignUpScreen();
+        } else if (settings.name == ForgotPasswordVerifyEmailScreen.name) {
+          widget = const ForgotPasswordVerifyEmailScreen();
+        } else if (settings.name == ForgotPasswordVerifyOtpScreen.name) {
+          widget = const ForgotPasswordVerifyOtpScreen();
+        } else if (settings.name == ResetPasswordScreen.name) {
+          widget = const ResetPasswordScreen();
+        } else if (settings.name == MainBottomNavScreen.name) {
+          widget = const MainBottomNavScreen();
+        }
+        return MaterialPageRoute(builder: (ctx) => widget);
+      },
+    );
   }
 }
